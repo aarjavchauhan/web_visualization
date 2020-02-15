7ce53ae5691aa4fcefa8f9d3f5e8b1d39c8b0cfd
Aarjav Chauhan <aarjavchauhan439@gmail.com>
2020-02-15 13:40:26 -0700

    grab links from text file and find hrefs in them, added history text file

--- web_crawl.py
+++ web_crawl.py
@@ -1,8 +1,33 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
-r = requests.get("https://berobinhood.com/")
-soup = BeautifulSoup(r.content, 'html.parser')
+listOfHistory = []
 
-for link in soup.find_all('a'):
-    print(link.get('href'))
+file = open("links.txt", "rt")
+while True:
+    # Get next line from file
+    line = file.readline()
+    # If line is empty then end of file reached
+    if not line:
+        break
+    regexMatch = re.match("(http://|ftp://|https://)([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?", line)
+    if bool(regexMatch):
+        listOfHistory.append(regexMatch.groups())
+file.close()
+
+for item in listOfHistory:
+    soup = BeautifulSoup(requests.get(item[0]+item[1]+item[2]).content, 'html.parser')
+    for link in soup.find_all('a'):
+        print(link.get('href'))
+    # # optimize code for internal and external links, should i categorize on each type?
+        # print(link.get('href')
+
+# r = requests.get(listOfHistory[0][0]+listOfHistory[0][1])
+# soup = BeautifulSoup(r.content, 'html.parser')
+
+# networkDict = {}
+
+# for link in soup.find_all('a'):
+# # optimize code for internal and external links, should i categorize on each type?
+#     print(link.get('href'))
