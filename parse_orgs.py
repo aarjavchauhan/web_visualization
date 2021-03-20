be0aa7711fe18248a35b0aba7a12f0d95e23bee5
pv <parshav@live.com>
2021-03-20 11:16:54 -0500

    p4

---
+++ parse_orgs.py
@@ -0,0 +1,21 @@
+import sys
+import json
+from urllib.parse import urlparse, urljoin
+
+file_name = sys.argv[1]
+url_set = set()
+
+with open(file_name) as file:
+    data = file.read()
+    pretty_json = json.loads(data)
+    nodes = pretty_json["nodes"]
+    for node in nodes:
+        if("org" in node["secondLevelDomain"]):
+            url = urlparse(node["name"])
+            url_set.add(url.netloc)
+
+print(url_set)
+with open(file_name.replace(".json", ".txt"), 'w') as f:
+    for item in url_set:
+        f.write(item)
+        f.write("\n")
